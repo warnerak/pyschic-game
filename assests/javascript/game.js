d93f54e7a052c153f5eed5475ec119d98d47ff13
unknown <warnerak@dukes.jmu.edu>
2019-03-02 08:48:25 -0500

    final push for pyschic game

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,66 +1,63 @@
-
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-  "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
-  "z"
-];
-var wins = 0;
-var losses = 0;
-var numGuesses = [9];
-var guessChoices = [];
-
-var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-  console.log(computerGuess)
-
-document.onkeyup = function(event) {
-  var userGuess = event.key;
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-  var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-  "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
-  "z"];
-
-  var userAnswers = [];
   
-    if (options.indexOf(userGuess) > -1) {
-      if (userGuess === computerGuess) {
-          wins++;
-          numGuesses = 9;
-          guessChoices = [];
-      }
-      else {
-          if (guessChoices.includes(userGuess)) {
-              var isDuplicate = false;
-              for (var i = 0; i < userAnswers.length; i++) { //for every previous answer
-                  if (currentAnswer == userAnswers[i]) { //check if the current answer is a previous one
-                  isDuplicate = true;
-                  break; //exit loop
-                  }
-              }
-              if (!isDuplicate) { //different than previous
-                userAnswers[userAnswers.length] = currentAnswer;
-              }
-          
-              else {
-              numGuesses--;
-              guessChoices.push(userGuess);
-          }
-      }
-  
-      if (numGuesses === 0) {
-          numGuesses = 9;
-          losses++;
-          guessChoices = [];
-      }
-  }  
-}
+    //create an array for the computer to make a choice
+		 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    
+    //create your variables for your wins, losses, number of guesses, and your choices so far
+		 var wins = 0;
+		 var losses = 0;
+		 var numGuesses = 10;
+     var guessChoices = [];
+     
+    //create the function for the computer to make its choice, console log the choice so you can see that it is working
+     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+     console.log(computerGuess)
 
-    var html
-    "<h1>The Psychic Game</h1>" +
-    "<p>Guess what letter I'm thinking!</p>" +
-    "<p>Wins: " + wins + "</p>" +
-    "<p>Losses: " + losses + "</p>" +
-    "<p>Guesses Left: " + numGuesses + "</p>" +
-    "<p>Your guesses so far: " + guessChoices.join (", ") + "</p>" ;
-    document.querySelector(".game").innerHTML = html;
-  }
+    //function to hit any button to start game
+		 document.onkeyup = function(event) {
 
+		 	var userGuess = event.key;
+
+		 	
+    //array for appropriate user guesses
+		 	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+		 	
+    //function to make sure each guess will be a string
+			if (options.indexOf(userGuess) > 0) {
+    
+    //if the userGuess is the computerGuess then wins go up, numGuess resets to 10, and the guesschoices reset
+				if (userGuess === computerGuess) {
+					wins++;
+					numGuesses = 10;
+					guessChoices = [];
+				}
+    
+    //if the userGuess does not equal the computerGuess the numberGuesses go down, and the userguess is put into the guessChoices
+				if (userGuess != computerGuess) {
+					numGuesses --;
+					guessChoices.push(userGuess);
+				}
+    
+    //if the numberGuesses runs out the increase the losses by one and reset game
+				if (numGuesses === 0) {
+
+				numGuesses = 10;
+				losses ++;
+				guessChoices = [];
+
+				
+			}
+
+    //sets up the javascript with the html tags
+			var html = 
+			"<h1> The Psychic Game </h1>" +
+			"<p>Guess what letter I'm thinking of!</p>" +
+			"<p>Wins: " + wins + "</p>" +
+			"<p>Losses: " + losses + "</p>" +
+			"<p>Guesses Left: " + numGuesses + "</p>" +
+			"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+
+			document.querySelector("#game").innerHTML = html;
+
+			
+			}
+		};
